d59ca28e91843b379adcd858b1a48aa8e6212c7e
ol-web <ol_web@outlook.com>
2023-03-06 09:22:56 +0100

    Add javascript code guide rules

--- index.js
+++ index.js
@@ -3,18 +3,6 @@ const defaultConfig = {
   extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
   plugins: ['import'],
   rules: {
-    'no-multiple-empty-lines': ['error', { max: 2 }],
-    'import/newline-after-import': ['warn', { count: 1 }],
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/ban-ts-comment': 'warn',
-    '@typescript-eslint/no-shadow': 'warn',
-    '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/no-inferrable-types': 'warn',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/array-type': 'warn',
-    '@typescript-eslint/method-signature-style': 'warn',
-    '@typescript-eslint/consistent-type-assertions': ['warn', { assertionStyle: 'as', objectLiteralTypeAssertions: 'allow-as-parameter' }],
     'no-restricted-syntax': [
       'warn',
       {
@@ -30,6 +18,42 @@ const defaultConfig = {
         message: 'Prefer declaring property type with optional ? operator instead of undefined',
       },
     ],
+    'arrow-body-style': ['warn', 'as-needed'],
+    curly: ['warn', 'multi', 'consistent'],
+    'spaced-comment': ['warn', 'always'],
+    'no-empty': 'warn',
+    'prefer-destructuring': ['warn', { object: false, array: true }],
+    'max-params': ['warn', { max: 2 }],
+    'no-multiple-empty-lines': ['error', { max: 2 }],
+    'unicorn/prefer-ternary': 'warn',
+    'import/newline-after-import': ['warn', { count: 1 }],
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+        alphabetize: {
+          order: 'asc',
+        },
+        pathGroups: [
+          {
+            pattern: '@/**',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    '@typescript-eslint/no-shadow': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/array-type': 'warn',
+    '@typescript-eslint/method-signature-style': 'warn',
+    '@typescript-eslint/consistent-type-assertions': ['warn', { assertionStyle: 'as', objectLiteralTypeAssertions: 'allow-as-parameter' }],
   },
 };
 
