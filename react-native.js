396c86b01206a499135ff45dfd9dd7431028b5cf
Szymon Dolnik <szymon2407@hotmail.com>
2024-04-17 16:16:47 +0200

    refactor(react native): Use newest eslint config from react native team

--- react-native.js
+++ react-native.js
@@ -1,6 +1,6 @@
 /** @type {import("eslint").Linter.Config}  */
 const reactNative = {
-  extends: ['@react-native-community', '@sparing-software/eslint-config'],
+  extends: ['@react-native', '@sparing-software/eslint-config'],
   plugins: ['import'],
   settings: {
     'import/resolver': {
